fd5167df742250848ce5d5005031736576f503d8
python anywhere <buckosegueda@gmail.com>
2023-10-31 01:34:41 +0000

    new commit

--- blackJack/blackjack.py
+++ blackJack/blackjack.py
@@ -1,6 +1,6 @@
 '''
 blackjack
-5 deck shoe. When there are less than 2 decks, shoe is pulled and replaced. Dealer draws while hand is below hard 17. 
+5 deck shoe. When there are less than 2 decks, shoe is pulled and replaced. Dealer draws while hand is below hard 17.
 '''
 
 import random
@@ -19,7 +19,7 @@ def blackJack(money):
   #shuffle the shoe. all cards will be drawn from this shuffled shoe.
   random.shuffle(deck)
 
-  #function evaluates a hand and returns its value. 
+  #function evaluates a hand and returns its value.
   #Dealer is a bool variable so that after the draw, the player only sees one card the dealer has.
   def handValues(hand, handTotal, dealer=False):
     #totals up all cards in hand by referencing the type of card and its value in the cardValues dictionary.
@@ -35,12 +35,12 @@ def blackJack(money):
     if dealer == True:
       handTotal=cardValues.get(hand[0][0])
     return(handTotal)
-  
+
   #Function that pulls another card from the deck and adds it to the player or dealers hand.
   def hit(hand):
     hand.append(deck.pop())
     return(hand)
-  
+
   def gameLoop(deck, money):
     #Initializing variables that will be used throughout the game.
     dealerHand=[]
@@ -50,33 +50,33 @@ def blackJack(money):
     playerMoney=money
     #Bool statement controls the core gameplay loop. Since the game was started by the player, its default is False.
     end=False
-    
+
     print(f'You have ${playerMoney}')
     bet = int(input('what do you bet? $'))
-    #tests if player made a valid bet. Valid bets must be greater than 0 and less than what the player has. 
+    #tests if player made a valid bet. Valid bets must be greater than 0 and less than what the player has.
     if bet > playerMoney or bet < 0:
       print('You cant do that')
       return(gameLoop(deck, playerMoney))
     #Subtracts bet from player's money
     else:
       playerMoney-=bet
-    
+
     #initial deal. Modulo statement is there so that dealer and player alternately receive cards.
     for i in range(4):
       if i % 2 != 0:
         playerHand.append(deck.pop())
       else:
         dealerHand.append(deck.pop())
-    
+
     #reassign player and dealer hand totals.
     playerTotal=handValues(playerHand,playerTotal)
     dealerTotal=handValues(dealerHand,dealerTotal)
-  
+
     while end == False:
       #print statements display the players hand and one card of the dealer.
       print(f'Player has {playerHand} for {playerTotal}')
       print(f'Dealer has {dealerHand[0]} for {handValues(dealerHand,dealerTotal,True)}')
-      
+
       #below are automatic win/loss conditions.
       if dealerTotal == 21:
         if playerTotal != 21:
@@ -93,17 +93,17 @@ def blackJack(money):
         else:
           result='Push'
         end=True
-      
+
       #Player has not auto won/loss. Must decide to hit or stay.
       else:
         decision = input('Would you like to hit? ')
-        #decision to hit adds a new card to players hand and kicks back to beginning of while loop, to test if player busted. 
+        #decision to hit adds a new card to players hand and kicks back to beginning of while loop, to test if player busted.
         if decision == 'y':
           playerHand = hit(playerHand)
           #Reset the player total so that it isn't counted twice when it is updated on the next line.
           playerTotal=0
           playerTotal= handValues(playerHand, playerTotal)
-        
+
         #decision ends players moves, displays dealers hand and will evaluate if dealer hits or not.
         if decision == 'n':
           print(f'Dealer hand has {dealerHand} for {dealerTotal}')
@@ -113,7 +113,7 @@ def blackJack(money):
             dealerTotal=0
             dealerTotal=handValues(dealerHand, dealerTotal)
             print(f'Dealer draws {dealerHand[-1]} for {dealerTotal}')
-          
+
           #following are win loss conditions.
           if playerTotal > dealerTotal or dealerTotal > 21:
             result='You win!'
@@ -129,12 +129,12 @@ def blackJack(money):
       playerMoney+=(bet*2)
     elif result == "Push":
       playerMoney+=bet
-    
+
     #final output of hand.
-    print(f'Player hand is {playerHand} for {playerTotal}')        
+    print(f'Player hand is {playerHand} for {playerTotal}')
     print(f'Dealer hand is {dealerHand} for {dealerTotal}')
     print(result)
-    
+
     #ask the player if they want to play again.
     if input(f'You have ${playerMoney} would you like to play again? ') == 'y':
       #If they do but the deck is smaller than 2 decks. Reset the shoe.
@@ -146,8 +146,8 @@ def blackJack(money):
         gameLoop(deck, playerMoney)
     #ends game
     else:
-      print(f'You finished with ${playerMoney}')  
+      print(f'You finished with ${playerMoney}')
       return()
   gameLoop(deck, money)
-      
+
 blackJack(100)
